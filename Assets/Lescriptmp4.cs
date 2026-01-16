d0f0e47d9b110464b5889cc695ed45770c9cf1df
Coffe_Crab <crablifedesign@gmail.om>
2026-01-16 16:13:21 +0100

    Add - variable de mon vié

--- Assets/Lescriptmp4.cs
+++ Assets/Lescriptmp4.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Lescriptmp4 : MonoBehaviour
 {
+    public bool orWhat;
+    private int integrityValue;
+
+    public float numberOfOrWhats;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
